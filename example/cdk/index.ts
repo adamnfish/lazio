7c9f5a6993f740da4baf2b3e680afe4b0933c775
adamnfish <adamnfish@gmail.com>
2019-08-01 00:37:16 +0100

    Add CDK code for deployment of example

---
+++ example/cdk/index.ts
@@ -0,0 +1,27 @@
+import apigateway = require('@aws-cdk/aws-apigateway'); 
+import lambda = require('@aws-cdk/aws-lambda');
+import cdk = require('@aws-cdk/core');
+
+export class LazioLambda extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id);
+
+    const lambdaFn = new lambda.Function(this, 'lazio-example', {
+      code: lambda.Code.asset("../target/universal/example.zip"),
+      handler: 'com.adamnfish.lazio.Lambda',
+      runtime: lambda.Runtime.JAVA_8,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(20),
+      environment: {}
+    });
+    
+    new apigateway.LambdaRestApi(this, 'itemsApi', {
+      handler: lambdaFn,
+      restApiName: 'Lazio example'
+    });
+  }
+}
+
+const app = new cdk.App();
+new LazioLambda(app, 'LazioLambda');
+app.synth();
