8e20c3611179e3da8c3a4f4de9ac837d960e3b6e
adamnfish <adamnfish@gmail.com>
2019-08-15 23:56:05 +0100

    Dev server listens for ctrl+d to terminate
    Also tidies the file a little

--- example/devServer/src/main/scala/com/adamnfish/lazio/DevServer.scala
+++ example/devServer/src/main/scala/com/adamnfish/lazio/DevServer.scala
@@ -1,29 +1,46 @@
 package com.adamnfish.lazio
 
-import com.amazonaws.services.lambda.runtime.{ClientContext, CognitoIdentity, LambdaLogger, Context => LambdaContext}
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
-import zio.DefaultRuntime
-import zio.console.Console
 import io.javalin.Javalin
 import io.javalin.http.{Context, HandlerType}
+import zio.DefaultRuntime
+
+import scala.Console._
+import scala.io.StdIn
 
 
 object DevServer {
-  val handler = new Lambda {
-    // Override these to provide dev dependencies
-    override type Environment = Console
+
+  val devLambda = new Lambda {
+    // Override the runtime to provide dev dependencies for the program's execution
     override val runtime = new DefaultRuntime {}
   }
 
+
   def main(args: Array[String]): Unit = {
     val app = Javalin.create
-    // add handlers for all supported HTTP methods
-    // to mimic API Gateway's proxy integration
-    HandlerType.values().foreach { handlerType =>
+    // handlers for HTTP methods to mimic API Gateway's proxy integration
+    List(
+      HandlerType.GET, HandlerType.POST,
+      HandlerType.PUT, HandlerType.DELETE,
+      HandlerType.PATCH, HandlerType.TRACE,
+    ).foreach { handlerType =>
       app.addHandler(handlerType, "*", handleRequest)
     }
-
     app.start(7000)
+
+    println(s"$BOLD$GREEN_B${WHITE}Lambda dev server running: ${RED}ctrl+d$WHITE to stop$RESET")
+
+    // listen for `ctrl+d` to stop the dev server
+    var running = true
+    while(running){
+      StdIn.readLine match {
+        case x if x == null =>
+          app.stop()
+          running = false
+        case _ =>
+      }
+    }
   }
 
   def handleRequest(ctx: Context): Unit = {
@@ -35,8 +52,8 @@ object DevServer {
       .withIsBase64Encoded(false)
     val lambdaContext = new FakeLambdaContext
 
-    val program = handler.handle(requestEvent, lambdaContext)
-    val response = handler.runtime.unsafeRun(program)
+    val program = devLambda.handle(requestEvent, lambdaContext)
+    val response = devLambda.runtime.unsafeRun(program)
 
     ctx.status(response.statusCode)
     response.headers.foreach { case (k, v) =>
